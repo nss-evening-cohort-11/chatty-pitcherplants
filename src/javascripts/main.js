5e08037b67be6db24b0ccde1265199340e53be54
Olamide Oladinni <olajide1987@gmail.com>
2020-03-12 00:48:16 -0500

    changes

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,7 +5,11 @@ import displayMessages from './components/displayMessages';
 
 const buttonEvents = () => {
   $('#incoming-message').on('click', '#send-button', add-message);
-  $('#incoming-message').on('change', '#iris', );
+  $('#incoming-message').on('change', '#brandi',add-message);
+  $('#incoming-message').on('change', '#logan',add-message);
+  $('#incoming-message').on('change', '#iris',add-message);
+  $('#incoming-message').on('change', '#becca',add-message);
+  $('#incoming-message').on('change', '#helen',add-message);
 
 };
 
