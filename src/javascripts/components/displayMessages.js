85ec9984c20aee2e726c34e067dde3f0e79a7610
David Everett <davideverett1989@gmail.com>
2020-03-13 23:41:10 -0500

    Finished timestamp.

--- src/javascripts/components/displayMessages.js
+++ src/javascripts/components/displayMessages.js
@@ -23,7 +23,7 @@ const addMessage = () => {
   const message = document.getElementById('user-message').value;
 
   const messageObject = {
-    date: Date.now(),
+    date: moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a'),
     name: $('input[name="userSelection"]:checked').val(),
     message,
     id: 'User id + index in message array',
