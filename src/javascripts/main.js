182cdd70fe0de6dd4ae49a4d68a1eb633b6d98fe
Olamide Oladinni <olajide1987@gmail.com>
2020-03-14 12:53:40 -0500

    delete radial events

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -11,11 +11,6 @@ const buttonEvents = () => {
   const sendBtn = document.getElementById('send-button');
   sendBtn.addEventListener('click', displayMessage.addMessage);
   $('#incoming-message').on('click', '#send-button', displayMessage.addMessage);
-  $('#incoming-message').on('change', '#brandy', displayMessage.addMessage);
-  $('#incoming-message').on('change', '#logan', displayMessage.addMessage);
-  $('#incoming-message').on('change', '#iris', displayMessage.addMessage);
-  $('#incoming-message').on('change', '#becca', displayMessage.addMessage);
-  $('#incoming-message').on('change', '#helen', displayMessage.addMessage);
 };
 
 const init = () => {
