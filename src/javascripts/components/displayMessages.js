ff375ab16fae268fc38bbb11ae4ce7bb0166fc47
Olamide Oladinni <olajide1987@gmail.com>
2020-03-15 12:37:17 -0500

    addLargeText

--- src/javascripts/components/displayMessages.js
+++ src/javascripts/components/displayMessages.js
@@ -3,10 +3,12 @@ import util from '../helpers/util';
 import messageData from '../helpers/data/messageData';
 
 const displayAllMessages = () => {
+  const largeTextRadioButton = document.querySelector('input[name="largeTextRadioButton"]');
   const messages = messageData.getMessages();
   let domString = '';
-
+  const isLargeChecked = largeTextRadioButton.checked;
   messages.forEach((userMessage) => {
+    domString += `<div class="${isLargeChecked === true ? 'large-text' : ''}">`;
     domString += '<div class="card my-3" style="width: 18rem;">';
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">${userMessage.name}</h5>`;
